4f840ec76200f1c1cfab15256d668259221083d4
Красавчик <stech999@ya.ru>
2023-08-23 01:00:44 +0300

    обновление решения

---
+++ lesson8/task38.py
@@ -0,0 +1,68 @@
+# на Отлично в одного человека надо сделать консольное приложение Телефонный справочник с внешним
+# хранилищем информации, и чтоб был реализован основной функционал -
+# просмотр, сохранение, импорт, поиск, удаление.
+# Задача 38: Дополнить телефонный справочник возможностью изменения и удаления данных.
+# Пользователь также может ввести имя или фамилию, и Вы должны реализовать функционал для изменения и удаления данных
+# для отлично в группах надо выполнить или ТГ бот или ГУИ (это когда кнопочки и поля ввода как в Виндовс приложениях)
+# или БД ГУИ можно сделать просто на EasyGUI или Tkinter
+
+import sqlite3 as sq
+
+with sq.connect("contacts.db") as con:
+    cur = con.cursor()
+    cur.execute("""
+    """)
+
+def create_table():
+    cur.execute("""CREATE TABLE IF NOT EXISTS contact (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    phone TEXT,
+    dr TEXT,
+    mail TEXT
+)""")
+
+
+def add_contact():
+    name = input("Имя: ")
+    phone = input("Номер телефона: ")
+    dr = input("Введите дату рождения: ")
+    mail = input("Введите вашу почту: ")
+    cur.execute(f"INSERT INTO contact (name, phone, dr, mail) VALUES('{name}','{phone}','{dr}','{mail}')")
+    print("Контакт успешно сохранен.")
+    con.commit()
+
+
+def del_contact():
+    names = input("Введите имя для удаления контакта: ")
+    sql = f"DELETE FROM contact WHERE name = '{names}'"
+    print("Контакт успешно удален.")
+    con.execute(sql)
+    con.commit()
+
+def show_data():  # показываем все записи таблицы users
+    cur.execute("select * from contact")
+    for row in cur.fetchall():
+        print(row)
+
+create_table()
+
+while True:
+    print("1. Добавить контакт.\n"
+          "2. Удалить контакт.\n"
+          "3. Показать все контакты.")
+
+    command = input("Введите номер команды: ")
+    if command == 1:
+        add_contact()
+        break
+    elif command == 2:
+        del_contact()
+        break
+    elif command == 3:
+        show_data()
+        break
+    else:
+        print("Вы ввели не существующий номер команды. ")
+
+con.close()
